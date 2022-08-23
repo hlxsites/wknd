00ac8a258e3e5a1cc7869616b9a00b4143fc1f77
Tobias Bocanegra <tripod@adobe.com>
2022-08-23 16:34:47 +0200

    chore: breadcrumb

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -615,6 +615,34 @@ function buildHeroBlock(main) {
   }
 }
 
+function buildBreadCrumbs(main) {
+  const $nav = document.createElement('nav');
+  $nav.classList.add('breadcrumb');
+  const $ul = document.createElement('ul');
+  $nav.append($ul);
+  const trail = [{
+    text: 'Adventures',
+    link: '/adventures',
+  }, {
+    text: 'Bali Surf',
+  }];
+  while (trail.length) {
+    const step = trail.shift();
+    const $li = document.createElement('li');
+    $ul.append($li);
+    let $wrap = $li;
+    if (step.link) {
+      $wrap = document.createElement('a');
+      $wrap.href = step.link;
+      $li.append($wrap);
+    }
+    const $span = document.createElement('span');
+    $wrap.append($span);
+    $span.textContent = step.text;
+  }
+  main.insertBefore($nav, main.firstElementChild);
+}
+
 function loadHeader(header) {
   const headerBlock = buildBlock('header', '');
   header.append(headerBlock);
@@ -635,7 +663,9 @@ function loadFooter(footer) {
  */
 function buildAutoBlocks(main) {
   try {
-    buildHeroBlock(main);
+    // disable hero block for now
+    // buildHeroBlock(main);
+    buildBreadCrumbs(main);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
