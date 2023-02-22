e4ca3f7fed517cc145cbfd32831ffd34aa429285
Markus Haack <mhaack@adobe.com>
2023-02-22 17:23:30 +0100

    Add import script

---
+++ tools/importer/import.js
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* global WebImporter */
+/* eslint-disable no-console, class-methods-use-this */
+
+const createMetadata = (main, document) => {
+  const meta = {};
+
+  const title = document.querySelector('title');
+  if (title) {
+    meta.Title = title.innerHTML.replace(/[\n\t]/gm, '');
+  }
+
+  const desc = document.querySelector('[property="og:description"]');
+  if (desc) {
+    meta.Description = desc.content;
+  }
+
+  const img = document.querySelector('[property="og:image"]');
+  if (img && img.content) {
+    const el = document.createElement('img');
+    el.src = img.content;
+    meta.Image = el;
+  }
+
+  const block = WebImporter.Blocks.getMetadataBlock(document, meta);
+  main.append(block);
+
+  return meta;
+};
+
+const transformQuote = (document) => {
+  document.querySelectorAll('.cmp-text--quote').forEach((quote) => {
+    const cells = [['Blockquote']];
+
+    const quoteText = quote.querySelector('blockquote');
+    cells.push([quoteText.textContent]);
+
+    const quoteAuthor = quote.querySelector('p > u');
+    if (quoteAuthor) {
+      cells.push([quoteAuthor.textContent]);
+    }
+
+    const table = WebImporter.DOMUtils.createTable(cells, document);
+    quote.replaceWith(table);
+  });
+};
+
+export default {
+  /**
+   * Apply DOM operations to the provided document and return
+   * the root element to be then transformed to Markdown.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @returns {HTMLElement} The root element to be transformed
+   */
+  transformDOM: ({
+    // eslint-disable-next-line no-unused-vars
+    document, url, html, params,
+  }) => {
+    // define the main element: the one that will be transformed to Markdown
+    const main = document.body;
+
+    // use helper method to remove header, footer, etc.
+    WebImporter.DOMUtils.remove(main, ['header', 'footer', 'aside', '.breadcrumb', '.buildingblock']);
+
+    // create the metadata block and append it to the main element
+    createMetadata(main, document);
+
+    // convert all blocks
+    [
+      transformQuote,
+    ].forEach((f) => f.call(null, document));
+
+    return main;
+  },
+
+  /**
+   * Return a path that describes the document being transformed (file name, nesting...).
+   * The path is then used to create the corresponding Word document.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @return {string} The path
+   */
+  generateDocumentPath: ({
+    // eslint-disable-next-line no-unused-vars
+    document, url, html, params,
+  }) => WebImporter.FileUtils.sanitizePath(
+    new URL(url).pathname.replace(/\.html$/, '').replace(/\/$/, ''),
+  ),
+};
