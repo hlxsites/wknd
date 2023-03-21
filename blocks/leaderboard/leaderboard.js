ccaf9c5d13ac6c6ec54ea2105124809e5a5eec81
Abhishek Arora <aarora@adobe.com>
2023-03-21 18:34:44 +0530

    Add leaderboard block

---
+++ blocks/leaderboard/leaderboard.js
@@ -0,0 +1,53 @@
+import { fetchIndex } from '../../scripts/scripts.js';
+
+function parseDateTime(d, t) {
+    var d = new Date(d);
+    var time = t.match(/(\d+)(?::(\d\d))?\s*(p?)/);
+    d.setHours(parseInt(time[1]) + (time[3] ? 12 : 0));
+    d.setMinutes(parseInt(time[2]) || 0);
+    return d;
+}
+
+function dateTimeSort(a, b) {
+    var c = parseDateTime(a['Date'], a['Time']);
+    var d = parseDateTime(b['Date'], b['Time']);
+    return c-d; 
+}
+
+/**
+ *
+ * @param {HTMLElement} block
+ */
+export default async function decorate(block) {
+    // Fetch items from index
+    const indexContent = await fetchIndex('leaderboard');
+    const data = indexContent.data;
+
+    // Create the table
+    const table = document.createElement('table');
+    const headerRow = table.insertRow();
+    headerRow.insertCell().textContent = 'Date';
+    headerRow.insertCell().textContent = 'Name';
+    headerRow.insertCell().textContent = 'Time';
+    headerRow.insertCell().textContent = 'URL';
+
+    // Sort the rows by time
+   data.sort(dateTimeSort);
+
+    // Add the rows to the table
+    data.forEach(row => {
+        // console.log("row:", row); // debug
+        const tableRow = table.insertRow();
+        tableRow.insertCell().textContent = row['Date'];
+        tableRow.insertCell().textContent = row['Name'];
+        tableRow.insertCell().textContent = row['Time'];
+        tableRow.insertCell().innerHTML = `<a href="${row['URL']}">${row['URL']}</a>`;
+    });
+
+    // Add the table to the page
+    block.append(table);
+}
+
+
+
+
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -258,6 +258,46 @@ function loadDelayed() {
   // load anything that can be postponed to the latest here
 }
 
+export async function fetchIndex(indexFile, pageSize = 500) {
+  const handleIndex = async (offset) => {
+    const resp = await fetch(`/${indexFile}.json?limit=${pageSize}&offset=${offset}`);
+    const json = await resp.json();
+
+    const newIndex = {
+      complete: (json.limit + json.offset) === json.total,
+      offset: json.offset + pageSize,
+      promise: null,
+      data: [...window.index[indexFile].data, ...json.data],
+    };
+
+    return newIndex;
+  };
+
+  window.index = window.index || {};
+  window.index[indexFile] = window.index[indexFile] || {
+    data: [],
+    offset: 0,
+    complete: false,
+    promise: null,
+  };
+
+  // Return index if already loaded
+  if (window.index[indexFile].complete) {
+    return window.index[indexFile];
+  }
+
+  // Return promise if index is currently loading
+  if (window.index[indexFile].promise) {
+    return window.index[indexFile].promise;
+  }
+
+  window.index[indexFile].promise = handleIndex(window.index[indexFile].offset);
+  const newIndex = await (window.index[indexFile].promise);
+  window.index[indexFile] = newIndex;
+
+  return newIndex;
+}
+
 async function loadPage() {
   await loadEager(document);
   await loadLazy(document);
