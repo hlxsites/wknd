465aaf8125423264aa503f137c79a3d043cc7415
ravverma <ravverma@adobe.com>
2023-04-04 17:09:00 +0530

    updating to run demo

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,163 +1,73 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 import {
-  sampleRUM,
-  buildBlock,
   getMetadata,
-  loadHeader,
-  loadFooter,
-  decorateButtons,
-  decorateIcons,
-  decorateSections,
-  decorateBlocks,
-  decorateTemplateAndTheme,
-  waitForLCP,
+  init,
   loadBlocks,
-  loadCSS,
-  toClassName,
+  setPluginOptions,
+  withPlugin,
 } from './lib-franklin.js';
 
+window.isPlayerWioReady = true;
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information here
 
-// Define the custom audiences mapping for experimentation
-const EXPERIMENTATION_CONFIG = {
-  audiences: {
-    device: {
-      mobile: () => window.innerWidth < 600,
-      desktop: () => window.innerWidth >= 600,
-    },
-    visitor: {
-      new: () => !localStorage.getItem('franklin-visitor-returning'),
-      returning: () => !!localStorage.getItem('franklin-visitor-returning'),
-    },
-  },
+const screensOptions = {
+  condition: () => window.location.pathname.startsWith('/screens'),
+  itemDuration: getMetadata('item-duration') || undefined,
+  itemFit: getMetadata('item-fit') || undefined,
+  proofOfPlay: false, // window.location.hostname.endsWith('.hlx.live'),
+  type: (getMetadata('template') || 'multizone').toLowerCase(),
 };
 
-/**
- * Determine if we are serving content for the block-library, if so don't load the header or footer
- * @returns {boolean} True if we are loading block library content
- */
-export function isBlockLibrary() {
-  return window.location.pathname.includes('block-library');
-}
-
-/**
- * Convience method for creating tags in one line of code
- * @param {string} tag Tag to create
- * @param {object} attributes Key/value object of attributes
- * @param {HTMLElement | HTMLElement[] | string} children Child element
- * @returns {HTMLElement} The created tag
- */
-export function createTag(tag, attributes, children) {
-  const element = document.createElement(tag);
-  if (children) {
-    if (children instanceof HTMLElement
-      || children instanceof SVGElement
-      || children instanceof DocumentFragment) {
-      element.append(children);
-    } else if (Array.isArray(children)) {
-      element.append(...children);
-    } else {
-      element.insertAdjacentHTML('beforeend', children);
-    }
-  }
-  if (attributes) {
-    Object.entries(attributes).forEach(([key, val]) => {
-      element.setAttribute(key, val);
-    });
-  }
-  return element;
-}
-
-function buildHeroBlock(main) {
-  const h1 = main.querySelector('main > div > h1');
-  const picture = main.querySelector('main > div > p > picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
-  }
-}
+// Load the plugins for the project
+setPluginOptions('decorator', { loadCssThemes: true });
+await withPlugin('/plugins/perflogger/index.js');
+await withPlugin('/plugins/screens/index.js', screensOptions);
 
 /**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
 function buildAutoBlocks(main) {
-  try {
-    buildHeroBlock(main);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Auto Blocking failed', error);
-  }
-}
-
-function patchDemoBlocks(config) {
-  if (window.wknd.demoConfig.blocks && window.wknd.demoConfig.blocks[config.blockName]) {
-    const url = window.wknd.demoConfig.blocks[config.blockName];
-    const splits = new URL(url).pathname.split('/');
-    const [, owner, repo, , branch] = splits;
-    const path = splits.slice(5).join('/');
-
-    const franklinPath = `https://little-forest-58aa.david8603.workers.dev/?url=https://${branch}--${repo}--${owner}.hlx.live/${path}`;
-    return {
-      ...config,
-      jsPath: `${franklinPath}/${config.blockName}.js`,
-      cssPath: `${franklinPath}/${config.blockName}.css`,
-    };
-  }
-  return (config);
-}
-
-async function loadDemoConfig() {
-  const demoConfig = {};
-  const pathSegments = window.location.pathname.split('/');
-  if (window.location.pathname.startsWith('/drafts/') && pathSegments.length > 4) {
-    const demoBase = pathSegments.slice(0, 4).join('/');
-    const resp = await fetch(`${demoBase}/theme.json?sheet=default&sheet=blocks`);
-    if (resp.status === 200) {
-      const json = await resp.json();
-      const tokens = json.data || json.default.data;
-      const root = document.querySelector(':root');
-      tokens.forEach((e) => {
-        root.style.setProperty(`--${e.token}`, `${e.value}`);
-        demoConfig[e.token] = e.value;
-      });
-      demoConfig.tokens = tokens;
-      demoConfig.demoBase = demoBase;
-      const blocks = json.blocks ? json.blocks.data : [];
-      demoConfig.blocks = {};
-      blocks.forEach((block) => {
-        demoConfig.blocks[block.name] = block.url;
-      });
-
-      window.hlx.patchBlockConfig.push(patchDemoBlocks);
+  [...main.querySelectorAll('p>picture:only-child')].forEach((picture) => {
+    const p = picture.parentElement;
+    const div = p.parentElement;
+    const index = [...div.children].indexOf(p);
+    if (index > div.children) {
+      div.appendChild(picture);
+    } else {
+      div.insertBefore(picture, div.children[index]);
     }
-
-    if (!demoConfig.demoBase) {
-      const navCheck = await fetch(`${demoBase}/nav.plain.html`);
-      if (navCheck.status === 200) {
-        demoConfig.demoBase = demoBase;
-      }
-    }
-  }
-  window.wknd = window.wknd || {};
-  window.wknd.demoConfig = demoConfig;
+    p.remove();
+  });
 }
 
 /**
  * Decorates the main element.
  * @param {Element} main The main element
  */
-// eslint-disable-next-line import/prefer-default-export
-export function decorateMain(main) {
-  // hopefully forward compatible button decoration
-  decorateButtons(main);
-  decorateIcons(main);
-
+async function decorateMain(main) {
   buildAutoBlocks(main);
-  decorateSections(main);
-  decorateBlocks(main);
+  this.plugins.decorator.decorateButtons(main);
+
+  if (document.body.classList.contains('kiosk')) {
+    const kiosk = await import('./kiosk.js');
+    kiosk.default(main, {
+      ...this.plugins.decorator,
+      buildAutoBlocks,
+      loadBlocks,
+    });
+  }
 }
 
 /**
@@ -165,40 +75,9 @@ export function decorateMain(main) {
  */
 async function loadEager(doc) {
   document.documentElement.lang = 'en';
-  decorateTemplateAndTheme();
-
-  // load experiments
-  const experiment = toClassName(getMetadata('experiment'));
-  const instantExperiment = getMetadata('instant-experiment');
-  if (instantExperiment || experiment) {
-    const { runExperiment } = await import('./experimentation/index.js');
-    await runExperiment(experiment, instantExperiment, EXPERIMENTATION_CONFIG);
-  }
-
-  // load demo config
-  await loadDemoConfig();
-
   const main = doc.querySelector('main');
   if (main) {
-    decorateMain(main);
-    await waitForLCP(LCP_BLOCKS);
-  }
-}
-
-/**
- * Adds the favicon.
- * @param {string} href The favicon URL
- */
-export function addFavIcon(href) {
-  const link = document.createElement('link');
-  link.rel = 'icon';
-  link.type = 'image/png';
-  link.href = href;
-  const existingLink = document.querySelector('head link[rel="icon"]');
-  if (existingLink) {
-    existingLink.parentElement.replaceChild(link, existingLink);
-  } else {
-    document.getElementsByTagName('head')[0].appendChild(link);
+    decorateMain.call(this, main);
   }
 }
 
@@ -207,61 +86,21 @@ export function addFavIcon(href) {
  */
 async function loadLazy(doc) {
   const main = doc.querySelector('main');
-  await loadBlocks(main);
-
-  const { hash } = window.location;
-  const element = hash ? main.querySelector(hash) : false;
-  if (hash && element) element.scrollIntoView();
-
-  if (!isBlockLibrary()) {
-    loadHeader(doc.querySelector('header'));
-    loadFooter(doc.querySelector('footer'));
-  }
-
-  if (window.wknd.demoConfig.fonts) {
-    const fonts = window.wknd.demoConfig.fonts.split('\n');
-    fonts.forEach(async (font) => {
-      const [family, url] = font.split(': ');
-      const ff = new FontFace(family, `url('${url}')`);
-      await ff.load();
-      document.fonts.add(ff);
-    });
-  } else {
-    loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  }
-  addFavIcon(`${window.wknd.demoConfig.demoBase || window.hlx.codeBasePath}/favicon.png`);
-  sampleRUM('lazy');
-  sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
-  sampleRUM.observe(main.querySelectorAll('picture > img'));
-
-  // Load experimentation preview overlay
-  if (window.location.hostname === 'localhost' || window.location.hostname.endsWith('.hlx.page')) {
-    const preview = await import(`${window.hlx.codeBasePath}/tools/preview/preview.js`);
-    await preview.default();
-    if (window.hlx.experiment) {
-      const experimentation = await import(`${window.hlx.codeBasePath}/tools/preview/experimentation.js`);
-      experimentation.default();
-    }
-  }
-
-  // Mark customer as having viewed the page once
-  localStorage.setItem('franklin-visitor-returning', true);
+  // Remove any empty div from the markup to reduce the DOM size
+  [...main.querySelectorAll('div:empty')].forEach((div) => div.remove());
 }
 
 /**
  * loads everything that happens a lot later, without impacting
  * the user experience.
  */
-function loadDelayed() {
-  // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
+async function loadDelayed() {
   // load anything that can be postponed to the latest here
 }
 
-async function loadPage() {
-  await loadEager(document);
-  await loadLazy(document);
-  loadDelayed();
-}
-
-loadPage();
+init({
+  loadEager,
+  loadLazy,
+  loadDelayed,
+  lcpblocks: LCP_BLOCKS,
+});
