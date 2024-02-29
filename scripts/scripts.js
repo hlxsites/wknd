303a15c8dccd1d699d22cf0c8e587703591e212f
Julien Ramboz <ramboz@adobe.com>
2024-02-28 16:34:40 -0800

    chore: add support for all at.js version

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -184,7 +184,7 @@ async function loadEager(doc) {
   decorateTemplateAndTheme();
   window.targetGlobalSettings = {
     clientCode: 'sitesinternal',
-    cookieDomain: 'target-integration4--wknd--hlxsites.hlx.live',
+    cookieDomain: 'atjs--wknd--hlxsites.hlx.live',
     imsOrgId: '908936ED5D35CC220A495CD4@AdobeOrg',
     secureOnly: true,
     serverDomain: 'sitesinternal.tt.omtrdc.net',
