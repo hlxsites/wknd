ca14f79422d4cda6221f5271ca9293193b62d92d
vvedagiri <vedagiri@adobe.com>
2024-03-10 17:33:27 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -58,8 +58,8 @@ export default async function decorate(block) {
     });
     const topBar = document.createElement('div');
     topBar.classList.add('header-topbar');
-    block.prepend(topBar);
-    topBar.innerHTML = '<div>Sign In</div><div class="header-markets"><span class="icon icon-flag-us"></span>EN-US<span class="header-chevron-down"></span></div>';
+    //block.prepend(topBar);
+    //topBar.innerHTML = '<div>Sign In</div><div class="header-markets"><span class="icon icon-flag-us"></span>EN-US<span class="header-chevron-down"></span></div>';
     nav.prepend(hamburger);
     nav.setAttribute('aria-expanded', 'false');
     decorateIcons(block);
