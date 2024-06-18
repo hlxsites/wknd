8fb12441112aadbe1960acd6fe298b808f898280
Vitaly Tsaplin <tsaplin@adobe.com>
2024-06-18 13:12:57 +0200

    feat: load mystique playground

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -19,21 +19,3 @@ const analyticsConsent = localStorage.getItem('consent_status_ANALYTICS');
 if (analyticsConsent) {
   await analyticsSetConsent(analyticsConsent === 'ALLOW');
 }
-
-const mystique = async ({ detail }) => {
-  // const sk = detail.data;
-  // // your custom code from button.action goes here
-  console.log('Load Mystique here...', sk);
-};
-
-const sk = document.querySelector('helix-sidekick');
-if (sk) {
-  // sidekick already loaded
-  sk.addEventListener('custom:mystique', mystique);
-} else {
-  // wait for sidekick to be loaded
-  document.addEventListener('sidekick-ready', () => {
-    document.querySelector('helix-sidekick')
-      .addEventListener('custom:mystique', mystique);
-  }, { once: true });
-}
