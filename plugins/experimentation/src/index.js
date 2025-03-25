96dd35ef4ee5c8ee9352210b16b70e2202189ad7
Xinyi Feng <xfeng@adobe.com>
2025-03-25 08:43:11 -0700

    reload listener

--- plugins/experimentation/src/index.js
+++ plugins/experimentation/src/index.js
@@ -724,6 +724,12 @@ export async function loadLazy(document, options, context) {
       }
     }
   });
+
+  window.addEventListener('message', function(event) {
+    if (event.data && event.data.type === 'hlx:experimentation-window-reload' && event.data.action === 'reload') {
+        window.location.reload();
+    }
+  });
   // eslint-disable-next-line import/no-cycle
   const preview = await import('./preview.js');
   preview.default(document, pluginOptions, { ...context, getResolvedAudiences });
