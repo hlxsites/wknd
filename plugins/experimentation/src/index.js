0dc5710e9c4451d98cc616171b95e1305e0a73eb
Xinyi Feng <xfeng@adobe.com>
2025-03-25 08:32:46 -0700

    add reload event listener

--- plugins/experimentation/src/index.js
+++ plugins/experimentation/src/index.js
@@ -1088,13 +1088,13 @@ export async function loadLazy(document, options = {}) {
     }
   });
 
-  // window.addEventListener('message', function(event) {
-  //   console.log('Message received from iframe:', event.data);
-  //   if (event.data && event.data.type === 'EXPERIMENT_UPDATED' && event.data.action === 'RELOAD_PAGE') {
-  //       console.log('Reloading page due to experiment update');
-  //       window.location.reload();
-  //   }
-  // });
+  // event listener for rail iframe to reload the page
+  window.addEventListener('message', function(event) {
+    console.log('Message received from iframe:', event.data);
+    if (event.data && event.data.type === 'hlx:experimentation-window-reload' && event.data.action === 'reload') {
+        window.location.reload();
+    }
+  });
 
   const preview = await import(
     'https://opensource.adobe.com/aem-experimentation/preview.js'
