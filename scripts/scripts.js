2c3f4f92e58c0a18a4f66a044607bc821a475521
Julien Ramboz <ramboz@adobe.com>
2026-01-15 17:25:11 +0100

    chore: instrument martech lib and launch container

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -277,7 +277,7 @@ async function loadLazy(doc) {
     loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   }
   addFavIcon(`${window.wknd.demoConfig.demoBase || window.hlx.codeBasePath}/favicon.png`);
-  await import('./rum-to-analytics.js');
+  // await import('./rum-to-analytics.js');
   await martechLazy();
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
